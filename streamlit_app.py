12b1f95bef2f32d6ec853944214bf1492f821fb8
shubhamkaser-cd <shubham.kaser@collegedunia.com>
2022-03-15 19:22:12 +0530

    changes made to deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write('''
 ##### *Resume* 
 ''')
 
-image = Image.open('dp.png')
+image = Image.open('profpic.jpg')
 st.image(image, width=150)
 
 st.markdown('## Summary', unsafe_allow_html=True)
